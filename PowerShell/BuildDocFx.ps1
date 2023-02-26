155bb8801dffa6e01bc7d5ad599a0841d1ef4252
Peter Malmros Kretzmann <peter@kretzmann.dk>
2023-02-26 15:09:56 +0100

    New build

---
+++ PowerShell/BuildDocFx.ps1
@@ -0,0 +1,8 @@
+# To build the docs run:
+Set-Location Docfx
+
+# Build and creates docs folder as specified in the docfx.json
+docfx   
+
+# Create local host servcer - can preview the website on http://localhost:8080
+docfx docfx.json --serve -p 8081
