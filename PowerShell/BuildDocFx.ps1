70380422a71be89168c78a323931996ae4e6dcd9
Peter Malmros Kretzmann <peter@kretzmann.dk>
2023-02-27 12:56:41 +0100

    build

--- PowerShell/BuildDocFx.ps1
+++ PowerShell/BuildDocFx.ps1
@@ -7,7 +7,6 @@ Set-Location Docfx
 # Build and creates docs folder as specified in the docfx.json
 docfx   # use default template and not with modified favicon.icon and logo.svg
 docfx -t _exported_templates\default  # use modified template where favicon.icon and logo.svg is modified
-docfx -t just-the-docs
 # Create local host servcer - can preview the website on http://localhost:8081
 # docfx docfx.json --serve -p 8081
 
